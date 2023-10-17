71bfd6e835b336a187477213f2338e24131c7d85
Gelo La Madrid <hafallamadrid@gmail.com>
2023-10-17 08:32:14 +0800

    Initial Commit

---
+++ App.js
@@ -0,0 +1,37 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Text>Messaging Application!</Text>
+      </View>
+      <View style={styles.toolbar}></View>
+      <View style={styles.inputMethodEditor}></View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  content: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  inputMethodEditor: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  toolbar: {
+    borderTopWidth: 1,
+    borderTopColor: "rgba(0,0,0,0.04)",
+    backgroundColor: "white",
+  },
+});
