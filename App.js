ab41edde63436d8b9da653be2dc89a10414e867a
Gelo La Madrid <hafallamadrid@gmail.com>
2023-11-27 00:09:24 +0800

    Added Toolbar component

--- App.js
+++ App.js
@@ -1,57 +1,192 @@
-import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  Alert,
+  BackHandler,
+  Image,
+  Modal,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 import Status from "./components/Status";
 import MessageList from "./components/MessageList";
+import Toolbar from "./components/Toolbar";
 import {
   createImageMessage,
-  createLocationMessage,
   createTextMessage,
+  createLocationMessage,
 } from "./utils/MessageUtils";
 
 const App = () => {
-  const [messages] = useState([
-    createImageMessage("https://unsplash.it/300/300"),
+  const [messages, setMessages] = useState([
+    createImageMessage("https://unsplash.it/600/300"),
     createTextMessage("World"),
     createTextMessage("Hello"),
-    createLocationMessage({
-      latitude: 37.78825,
-      longitude: -122.4324,
-    }),
+    createLocationMessage({ latitude: 37.78825, longitude: -122.4324 }),
   ]);
 
+  const [fullscreenImage, setFullscreenImage] = useState(null);
+
+  const [isInputFocused, setIsInputFocused] = useState(false); // Track focus state
+
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      () => {
+        if (fullscreenImage) {
+          closeFullscreenImage();
+          return true;
+        }
+        return false;
+      }
+    );
+
+    return () => backHandler.remove();
+  }, [fullscreenImage]);
+
+  const handleDeleteMessage = (message) => {
+    const updatedMessages = messages.filter((m) => m !== message);
+    setMessages(updatedMessages);
+    closeFullscreenImage();
+  };
+
   const handlePressMessage = (message) => {
-    console.log("Message pressed", message);
+    if (message.type === "text") {
+      Alert.alert(
+        "Delete Message",
+        "Do you want to delete this message?",
+        [
+          { text: "Delete", onPress: () => handleDeleteMessage(message) },
+          { text: "Cancel", style: "cancel" },
+        ],
+        { cancelable: false }
+      );
+    } else if (message.type === "image") {
+      setFullscreenImage(message.uri);
+    }
+  };
+
+  const closeFullscreenImage = () => {
+    setFullscreenImage(null);
+  };
+
+  const renderFullscreenImage = () => {
+    return (
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={fullscreenImage !== null}
+        onRequestClose={closeFullscreenImage}
+      >
+        <View style={styles.fullscreenContainer}>
+          <Image
+            style={styles.fullscreenImage}
+            source={{ uri: fullscreenImage }}
+            resizeMode="contain"
+          />
+          <TouchableOpacity
+            style={styles.closeButton}
+            onPress={closeFullscreenImage}
+          >
+            <Text style={styles.closeButtonText}>Close</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.deleteButton}
+            onPress={() =>
+              handleDeleteMessage(
+                messages.find((m) => m.uri === fullscreenImage)
+              )
+            }
+          >
+            <Text style={styles.deleteButtonText}>Delete</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+    );
+  };
+
+  // Callbacks for Toolbar component
+  const handlePressToolbarCamera = () => {
+    // Handle camera press
+  };
+
+  const handlePressToolbarLocation = () => {
+    // Handle location press
+  };
+
+  const handleChangeFocus = (isFocused) => {
+    setIsInputFocused(isFocused);
+  };
+
+  const handleSubmit = (text) => {
+    const updatedMessages = [createTextMessage(text), ...messages];
+    setMessages(updatedMessages);
   };
 
   return (
     <View style={styles.container}>
-      <View style={styles.content}>
-        <MessageList messages={messages} onPressMessage={handlePressMessage} />
-      </View>
+      <Status />
+      <MessageList
+        messages={messages}
+        onPressMessage={handlePressMessage}
+        onDeleteMessage={handleDeleteMessage}
+      />
+      {renderFullscreenImage()}
+      {renderToolbar()}
     </View>
   );
+
+  function renderToolbar() {
+    return (
+      <Toolbar
+        isFocused={isInputFocused}
+        onSubmit={handleSubmit}
+        onChangeFocus={handleChangeFocus}
+        onPressCamera={handlePressToolbarCamera}
+        onPressLocation={handlePressToolbarLocation}
+      />
+    );
+  }
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
-    alignItems: "center",
+    alignItems: "flex-end",
     justifyContent: "center",
   },
-  content: {
+  fullscreenContainer: {
     flex: 1,
-    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black",
+  },
+  fullscreenImage: {
+    flex: 1,
     width: "100%",
+    height: "100%",
   },
-  inputMethodEditor: {
-    flex: 1,
-    backgroundColor: "white",
+  closeButton: {
+    position: "absolute",
+    top: 20,
+    right: 20,
+    padding: 10,
   },
-  toolbar: {
-    borderTopWidth: 1,
-    borderTopColor: "rgba(0,0,0,0.04)",
-    backgroundColor: "white",
+  closeButtonText: {
+    color: "white",
+    fontSize: 16,
+  },
+  deleteButton: {
+    position: "absolute",
+    top: 20,
+    left: 20,
+    padding: 10,
+  },
+  deleteButtonText: {
+    color: "white",
+    fontSize: 16,
   },
 });
 
--- components/MessageList.js
+++ components/MessageList.js
@@ -1,93 +1,96 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import { MessageShape } from "../utils/MessageUtils";
 
 const keyExtractor = (item) => item.id.toString();
 
-export default class MessageList extends React.Component {
-  static propTypes = {
-    messages: PropTypes.arrayOf(MessageShape).isRequired,
-    onPressMessage: PropTypes.func,
-  };
-
-  static defaultProps = {
-    onPressMessage: () => {},
-  };
-
-  renderMessageContent = (item) => {
-    const { onPressMessage } = this.props;
-
+const MessageList = ({ messages, onPressMessage }) => {
+  const renderMessageContent = (item) => {
     switch (item.type) {
       case "text":
         return (
           <View style={styles.messageRow}>
-            <View style={styles.messageBubble}>
-              <Text style={styles.text}>{item.text}</Text>
-            </View>
+            <TouchableOpacity onPress={() => onPressMessage(item)}>
+              <View style={styles.messageBubble}>
+                <Text style={styles.text}>{item.text}</Text>
+              </View>
+            </TouchableOpacity>
           </View>
         );
 
       case "image":
         return (
           <View style={styles.messageRow}>
-            <Image style={styles.image} source={{ uri: item.uri }} />
+            <TouchableOpacity onPress={() => onPressMessage(item)}>
+              <Image style={styles.image} source={{ uri: item.uri }} />
+            </TouchableOpacity>
           </View>
         );
 
       case "location":
-        if (item.coordinate) {
-          return (
-            <View style={styles.messageRow}>
-              <View style={styles.mapContainer}>
-                <MapView
-                  style={styles.map}
-                  initialRegion={{
-                    ...item.coordinate,
-                    latitudeDelta: 0.08,
-                    longitudeDelta: 0.04,
-                  }}
-                >
-                  <Marker coordinate={item.coordinate} />
-                </MapView>
-              </View>
-            </View>
-          );
-        } else {
-          return (
-            <Text style={styles.locationText}>Location data is missing</Text>
-          );
-        }
+        return (
+          <View style={styles.messageRow}>
+            <TouchableOpacity onPress={() => onPressMessage(item)}>
+              {item.coordinate ? (
+                <View style={styles.mapContainer}>
+                  <MapView
+                    style={styles.map}
+                    initialRegion={{
+                      ...item.coordinate,
+                      latitudeDelta: 0.08,
+                      longitudeDelta: 0.04,
+                    }}
+                  >
+                    <Marker coordinate={item.coordinate} />
+                  </MapView>
+                </View>
+              ) : (
+                <Text style={styles.locationText}>
+                  Location data is missing
+                </Text>
+              )}
+            </TouchableOpacity>
+          </View>
+        );
 
       default:
         return null;
     }
   };
 
-  render() {
-    const { messages } = this.props;
+  return (
+    <FlatList
+      style={styles.container}
+      inverted
+      data={messages}
+      renderItem={({ item }) => renderMessageContent(item)}
+      keyExtractor={keyExtractor}
+      keyboardShouldPersistTaps="handled"
+      contentContainerStyle={styles.contentContainer}
+    />
+  );
+};
 
-    return (
-      <FlatList
-        style={styles.container}
-        inverted
-        data={messages}
-        renderItem={({ item }) => this.renderMessageContent(item)}
-        keyExtractor={keyExtractor}
-        keyboardShouldPersistTaps="handled"
-        contentContainerStyle={styles.contentContainer}
-      />
-    );
-  }
-}
+MessageList.propTypes = {
+  messages: PropTypes.arrayOf(MessageShape).isRequired,
+  onPressMessage: PropTypes.func.isRequired,
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     overflow: "visible",
     backgroundColor: "#FFF",
-    marginTop: 40,
+    marginTop: 10,
   },
   contentContainer: {
     flexGrow: 1,
@@ -98,10 +101,10 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "flex-end",
     marginRight: 30,
-    marginBottom: 10,
+    marginBottom: 5,
   },
   messageBubble: {
-    backgroundColor: "blue",
+    backgroundColor: "#4285f4",
     padding: 10,
     borderRadius: 20,
   },
@@ -129,3 +132,5 @@ const styles = StyleSheet.create({
     fontStyle: "italic",
   },
 });
+
+export default MessageList;
